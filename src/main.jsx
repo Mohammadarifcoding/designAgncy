303e5baedcdffbd96277d0bce8848b0673ecd749
Mohammad Arif <programmermdarif@gmail.com>
2024-06-01 23:22:41 +0600

    Done the setup

---
+++ src/main.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import Exploring from './Exploring/Exploring';
+
+
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Exploring></Exploring>,
+  },
+  
+]);
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+   <RouterProvider router={router} />
+  </React.StrictMode>,
+)
