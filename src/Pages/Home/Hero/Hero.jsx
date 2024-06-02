d4e68c20e32fc7639d677e6e596a4b57d281b65f
Mohammad Arif <programmermdarif@gmail.com>
2024-06-02 22:49:41 +0600

    Added the banner

--- src/Pages/Home/Hero/Hero.jsx
+++ src/Pages/Home/Hero/Hero.jsx
@@ -1,14 +1,23 @@
 import Container from "../../../Shared/Container/Container";
 
-
 const Hero = () => {
-    return (
-        <Container>
-            <div className="bg-gray-100">
-fadsfsd
-            </div>
-        </Container>
-    );
+  return (
+    <Container>
+      <div className="bg-gray-100 py-20">
+        <h2 className="heading-1 text-center max-w-[1000px] mx-auto">
+          Unlimited POD Designs That Convert – Tailored Just for You
+        </h2>
+        <p className="description-1 max-w-[800px] mx-auto text-center my-8">
+          Unlock Creative Freedom with Our Flexible POD Design Subscriptions –
+          Pause or Cancel Anytime
+        </p>
+        <div className="flex justify-center  ">
+         <button className="px-10 bg-gradient-to-b from-black border-t-2 border-white shadow-normal-hover  to-gray-800 hover:shadow-custom-hover  transition-all duration-500 text-white py-5 rounded-lg ">See plans</button>
+        </div>
+      </div>
+    </Container>
+  );
 };
 
-export default Hero;Hero
+export default Hero;
+Hero;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}",'./node_modules/preline/preline.js',],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'xsm': '400px',
+      },
+      boxShadow: {
+        'custom-hover': '0 6px 13px rgba(0, 0, 0, 0.5)',
+        'normal-hover': '0 3px 6px rgba(0, 0, 0, 0.5)'
+      },
+    },
   },
-  plugins: [ require('preline/plugin'),],
+  plugins: [ ],
 };
