1f9f4deccad68cafb24d73cb39b014ce8ca3482b
Mohammad Arif <programmermdarif@gmail.com>
2024-06-03 16:30:05 +0600

    Updated the brand section design

--- src/Pages/Home/Hero/Hero.jsx
+++ src/Pages/Home/Hero/Hero.jsx
@@ -36,15 +36,18 @@ const Hero = () => {
           </div>
           <div className="flex justify-center text-[12px]">Available Now</div>
           <div className="flex justify-center my-10">
-            <Button className={'px-0 rounded-full'}>
-              <div className="flex  max-w-[700px]">
+            <Button className={'px-0  rounded-full '} OuterClassName={'rounded-full mx-4'}>
+              <div className="flex  lg:max-w-[700px] md:max-w-[500px] sm:max-w-[490px] xsm:max-w-[350px] max-w-[250px]">
                 <Marquee>
+                  <div className="flex">
                   {BrandName.map((item, idx) => (
                     <div key={idx} className="flex items-center gap-6">
                       <div className="">{item.name}</div>
                       <div className="bg-blue-500 w-[7px] h-[7px] mr-6 rotate-45"></div>
                     </div>
                   ))}
+                  </div>
+                 
                 </Marquee>
               </div>
             </Button>
