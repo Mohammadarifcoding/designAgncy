93f6b4350eac3b2195d6f50b9e0adb79337d0012
Mohammad Arif <programmermdarif@gmail.com>
2024-06-08 00:25:35 +0600

    removed the cd.doc

--- src/Pages/Home/AnyTask/AnyTask.jsx
+++ src/Pages/Home/AnyTask/AnyTask.jsx
@@ -9,10 +9,10 @@ const AnyTask = () => {
          <h2 className="heading-1 text-center max-w-[900px] mx-auto pb-10" >
          Request any design & we get it done for you
          </h2>
-           <div className="flex flex-wrap gap-5 justify-center items-center">
+           <div className="flex flex-wrap sm:gap-5 xsm:gap-3 gap-2 justify-center items-center">
             {
                 AnyTaskData.map((item)=>(
-                    <div className="px-6 py-5 rounded-lg bg-white text-xl  flex items-center  gap-2 shadow-lg cursor-pointer group"> <item.icon className="text-2xl group-hover:rotate-180 transition-all duration-1000"/> {item.title} </div>
+                    <div className="lg:px-6 lg:py-5 md:px-5 md:py-3 px-3 py-2  rounded-lg bg-white lg:text-xl md:text-lg sm:text-base text-sm  flex items-center  gap-2 shadow-lg cursor-pointer group"> <item.icon className="lg:text-2xl md:text-xl sm:text-lg text-base group-hover:rotate-180 transition-all duration-1000"/> {item.title} </div>
                 ))
             }
            </div>
