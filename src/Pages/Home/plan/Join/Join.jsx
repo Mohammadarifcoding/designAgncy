7e7c0890c0e98dcb4a12082c490e475ff6a12f80
Mohammad Arif <programmermdarif@gmail.com>
2024-06-22 07:43:41 +0600

    Added the plan component

---
+++ src/Pages/Home/plan/Join/Join.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Join = () => {
+  return (
+    <div className="mx-auto w-max min-w-[300px] rounded-xl transition-all duration-500 hover:shadow-custom-hover">
+      <div className="inner-shadow rounded-xl border-t-2 flex flex-col border-white bg-gradient-to-b from-black to-gray-800 px-10 py-5 text-white hover:from-[#292929] hover:to-gray-900">
+      {/* Slots */}
+        <div className='bg-[#FF90E7] py-3 px-5 text-black rounded-full w-max'>
+        Slots Available 
+        </div>
+      
+
+
+      </div>
+    </div>
+  );
+};
+
+export default Join;
