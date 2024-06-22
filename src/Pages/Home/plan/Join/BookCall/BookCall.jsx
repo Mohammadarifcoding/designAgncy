51f3c33bd9f181d269fba1f46d2068c66b8ead1e
Mohammad Arif <programmermdarif@gmail.com>
2024-06-22 08:23:38 +0600

    Added the calendy api

---
+++ src/Pages/Home/plan/Join/BookCall/BookCall.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const BookCall = () => {
+  return (
+    <a className='mt-20' target="_blank" href="https://calendly.com/nabirasek/30min">
+      <div className="inner-shadow rounded-xl border-t-2 border-white bg-gradient-to-b from-black to-gray-800  text-white transition-all duration-500 hover:from-[#292929] hover:to-gray-900 hover:shadow-custom-hover">
+        <div className=" flex w-auto flex-col items-center gap-3 rounded-lg border border-gray-700 p-4 shadow-xl sm:flex-row">
+          <div className="flex flex-col gap-3">
+            <h2 className="font-medium xsm:text-lg sm:text-xl">
+              Book a 15-min intro call
+            </h2>
+            <p className="text-[13px] font-medium xsm:text-sm sm:text-base">
+              Learn more about how Creative on Demand works and how it can help
+              you
+            </p>
+          </div>
+          <img
+            className="w-[25px]"
+            src="https://assets-global.website-files.com/5837424ae11409586f837994/65de12548dcbda2881cd8ff5_Group%201.svg"
+            alt=""
+          />
+        </div>
+      </div>
+    </a>
+  );
+};
+
+export default BookCall;
--- src/Pages/Home/plan/Join/Join.jsx
+++ src/Pages/Home/plan/Join/Join.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Button from "../../../../Shared/Button/Button";
+import BookCall from "./BookCall/BookCall";
 
 const Join = () => {
   return (
-    <div className="  max-w-[500px] rounded-xl">
+    <div className="max-w-[500px] rounded-xl">
       <div className="inner-shadow flex flex-col gap-7 rounded-xl border-t-2 border-white bg-gradient-to-b from-black to-gray-900 px-6 py-6 text-white sm:px-8 sm:py-8 md:px-10 md:py-10">
         {/* Slots */}
         <div className="w-max rounded-full bg-[#FF90E7] px-5 py-3 text-black">
@@ -13,23 +15,8 @@ const Join = () => {
           Join <br /> Creative On Demand
         </h2>
         {/* book call */}
+    <BookCall/>
 
-        <div className="mt-20 flex sm:flex-row flex-col items-center gap-3 rounded-lg border border-gray-700 p-4 shadow-xl">
-          <div className='flex flex-col gap-3'>
-            <h2 className="xsm:text-lg font-medium sm:text-xl">
-              Book a 15-min intro call
-            </h2>
-            <p className="font-medium sm:text-base xsm:text-sm text-[13px]">
-              Learn more about how Creative on Demand works and how it can help
-              you
-            </p>
-          </div>
-          <img
-            className="w-[25px]"
-            src="https://assets-global.website-files.com/5837424ae11409586f837994/65de12548dcbda2881cd8ff5_Group%201.svg"
-            alt=""
-          />
-        </div>
       </div>
     </div>
   );
