d18b6b17460b0669934783479a3eb677c67d7093
Mohammad Arif <programmermdarif@gmail.com>
2024-06-24 10:55:31 +0600

    Added the plan

---
+++ src/Pages/Home/plan/Pricing/Price/Price.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Button from './../../../../../Shared/Button/Button';
+
+const Price = ({ data }) => {
+    const {  id ,
+        plan,
+        price,
+        paymentLink,
+        features} = data
+  return (
+    <div className="mt-3">
+      <h2 className="flex items-end">
+        <span className="text-3xl font-[580] xsm:text-4xl md:text-5xl">
+          $4,99
+        </span>
+        /m
+      </h2>
+      <p className="mt-1 text-sm text-gray-800 xsm:text-base">
+        One request at a time. <br /> Pause or cancel anytime
+      </p>
+
+      <div className="my-7 h-[1px] bg-black "></div>
+        {/* Pricing description */}
+      <div className='mb-5'>
+       <h4 className=' text-gray-700  mb-6'>What's included</h4>
+      
+      <div className='grid xl:grid-cols-2 grid-cols-1'>
+      {features.map((item)=>(
+        <div className='text-gray-700  mb-1 flex gap-2 items-center'> <span className='bg-black h-[6px] w-[6px] rounded-full'></span> {item}</div>
+       ))}
+        </div> 
+      </div>
+      <div className='flex justify-center'>
+      <Button>Get Started</Button>
+      </div>
+       
+    </div>
+  );
+};
+
+export default Price;
--- src/Pages/Home/plan/Pricing/Pricing.jsx
+++ src/Pages/Home/plan/Pricing/Pricing.jsx
@@ -1,13 +1,14 @@
 import React,{useState} from "react";
-import BookCall from "../Join/BookCall/BookCall";
+
 import pricingPlans from "../../../../Data/PricingData";
+import Price from "./Price/Price";
 
 
 const Pricing = () => {
 
   const [selected,setSelected] = useState(0)
   return (
-    <div className="relative max-w-[400px]  rounded-xl bg-white">
+    <div className="relative max-w-[600px]  rounded-xl bg-white">
       {/* <img className='absolute right-0 rounded-r-xl' src="https://assets-global.website-files.com/5837424ae11409586f837994/65de149940ab9eb5eaf6a916_4.svg" alt="" / */}
       <div className="flex flex-col gap-7 rounded-xl border-t-2 border-white bg-gradient-to-b xsm:px-6 px-5 xsm:py-6 py-5 text-black sm:px-8 sm:py-8 md:px-10 md:py-10">
         <h2 className=" font-[580] md:text-5xl xsm:text-4xl text-3xl">MemberShip</h2>
@@ -20,7 +21,7 @@ const Pricing = () => {
           ))}
         </div>
         {/* book call */}
-        <BookCall />
+        <Price data={pricingPlans[0]}/>
       </div>
     </div>
   );
